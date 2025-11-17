5d96ab12917201f474e9356e1f0fafde53653818
zynexe <zuhayrraffa@gmail.com>
2025-11-17 09:42:22 +0700

    Remove unused React import in About page
    Cleaned up the import statement in About.tsx by removing the unused default React import, as only hooks are being used.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 import Navbar from "../components/Navbar";
 import MobileNavbar from "../components/MobileNavbar";
